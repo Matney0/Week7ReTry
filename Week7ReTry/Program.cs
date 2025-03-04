855f7eca1e76b503b75a352d76ce17ca9c9d4cc8
Matney0 <mlmoller21@amarillocollege.com>
2025-03-03 21:06:01 -0600

    Update Program.cs
    commited

--- Week7ReTry/Program.cs
+++ Week7ReTry/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,32 @@ namespace Week7ReTry
 {
     class Program
     {
+
+        //Method to counts the number of words in a text file
+        public static int CountWords(string fileName)
+        {
+            //Read the file
+            string content = System.IO.File.ReadAllText(fileName);
+            //Split the text into words
+            string[] words = content.Split(' ');
+            //Return the number of words
+            return words.Length;
+        }
+
         static void Main(string[] args)
         {
+            //string file = "C:\\Users\\user\\Desktop\\test.txt";
+            string fileName = "test.txt";
+
+            //Call the method to count the number of words in the file
+            int count = CountWords(fileName);
+
+            //return the number of words andd what the file saidd
+            Console.WriteLine("The file " + fileName + " contains " + count + " words.");
+
+            //read the text from the test file
+            string content = System.IO.File.ReadAllText(fileName);
+            Console.WriteLine("The file says: " + content);
         }
     }
 }
